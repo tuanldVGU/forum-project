2c35b4c887b431c53cf1c2ec90d2b689013fa0eb
tuanVGU <cs2015_tuan.ld@student.vgu.edu.vn>
2018-10-04 00:01:42 +0700

    ui update for index, create signin signup home page

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res,next){
-    res.render('index',{title:"welcome to Forted"});
+    res.render('index',{title:"welcome to Motor"});
 });
+
+router.get('/signup', function(req,res,next){
+    res.render('signup',{title:"Motor || Sign Up"});
+});
+
+router.get('/signin', function(req,res,next){
+    res.render('signin',{title:"Motor || Sign In"});
+});
+
+router.get('/home', function(req,res,next){
+    res.render('home',{title:"Motor || Home"});
+});
+
 
 module.exports = router;
