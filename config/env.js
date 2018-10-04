00668f897ed46d6234851f3b5b4eb49856576290
HaiDang9719 <haidanglhp1215@gmail.com>
2018-10-04 17:56:25 +0700

    working

--- config/env.js
+++ config/env.js
@@ -10,7 +10,7 @@ const defaultConfig = {
   }, {
     username: 'demo',
     password: 'demo',
-    email: 'demo@team1.tech',
+    email: 'demo@team1.com',
     userType: 'user',
   }],
 };
@@ -18,33 +18,14 @@ const defaultConfig = {
 const config = {
   test: {
     mongodb: 'mongodb://localhost:27017/forum-test',
-    redis: {
-      host: 'localhost',
-      port: 6379,
-      db: 0,
-    },
     domain: 'http://localhost:8080',
   },
   dev: {
     mongodb: 'mongodb://localhost:27017/forum-dev',
-    redis: {
-      host: 'localhost',
-      port: 6379,
-      db: 0,
-    },
-    logDir: '/var/tmp/storage/logs',
-    uploadDir: '/var/tmp/storage',
-    exportDir: '/var/tmp/storage/image',
-    tempDir: '/var/tmp/storage/temp',
     domain: 'http://localhost:8080',
   },
   prod: {
     mongodb: 'mongodb://mongod:27017/forum-prod',
-    redis: {
-      host: 'redis',
-      port: 6379,
-      db: 0,
-    },
     domain: '',
   },
 };
