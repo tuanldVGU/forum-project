e574b90417e9b6688f3f2e88b9649f70f7b856aa
HaiDang9719 <haidanglhp1215@gmail.com>
2018-10-08 12:36:40 +0700

    working

--- seed.js
+++ seed.js
@@ -1,3 +1,19 @@
 require('./init');
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
+
+const user = mongoose.model('user');
+const category = mongoose.model('category');
+const comment = mongoose.model('comment');
+const subcomment = mongoose.model('subcomment');
+const post = mongoose.model('post');
+user.deleteMany({}).exec()
+  .then(() => category.deleteMany({}).exec())
+  .then(() => comment.deleteMany({}).exec())
+  .then(() => subcomment.deleteMany({}).exec())
+  .then(() => post.deleteMany({}).exec())
+  .then(() => user.create({
+    username: '',
+    
+  }))
+
