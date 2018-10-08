28dbe9fb4e8740e6d0932f3b229216c4774416de
tuanldVGU <37131827+tuanldVGU@users.noreply.github.com>
2018-10-08 15:48:18 +0700

    fix server

--- server.js
+++ server.js
@@ -46,7 +46,10 @@ app.use('/forum',thread);
 app.use('/auth',auth);
 //app.use('/service', require('./routes/main'));
 // Start the server
-const PORT = server.listen(process.env.PORT || 8080);
-http.createServer(app).listen(PORT,function(){
-	console.log("App running on port "+ PORT);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, function(){
+	 console.log("App is running on port " + PORT);
 });
+// http.createServer(app).listen(PORT,function(){
+// 	console.log("App running on port "+ PORT);
+// });
