4022c3aa4a355c2e2baec75ac7401e3b469121e0
HaiDang9719 <haidanglhp1215@gmail.com>
2018-10-16 22:21:58 +0700

    add create comment service

--- services/commentService.js
+++ services/commentService.js
@@ -1,16 +1,22 @@
 const _ = require('lodash');
 const mongoose = require('mongoose');
 
-const comment = mongoose.model('comment');
+const comments = mongoose.model('comment');
+const posts = mongoose.model('post');
 
 const { ObjectId } = mongoose.Types;
 class commentService {
   static getDetail(postId) {
-    return comment.find({post: postId}).exec();
+    return comments.find({post: postId}).exec();
   }
   static getSumComment(postId){
   console.log(postId);
-    return comment.countDocuments({ post: ObjectId(postId) }).exec();
+    return comments.countDocuments({ post: ObjectId(postId) }).exec();
+  }
+  static createComment({ post, user, content }){
+    return posts.findOneAndUpdate({_id: ObjectId(post)}, { $inc: { numOfComment:1 } , recentComment: content}, {new: true }).exec()
+      .then(() => comments.create({ post: post, user: user, content: content }))
+      ;
   }
 
 }
