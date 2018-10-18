eb1c31f28291b440c71704e1c37f581d07387a4a
HaiDang9719 <haidanglhp1215@gmail.com>
2018-10-18 11:44:11 +0700

    add create subcomment service

--- models/subComment.js
+++ models/subComment.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
   comment: { type: mongoose.Schema.Types.ObjectId, ref: 'comment', default: undefined },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'post', default: undefined },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', default: undefined },
   content: { type: String, require: true }
 
--- services/subCommentService.js
+++ services/subCommentService.js
@@ -1,13 +1,24 @@
 const _ = require('lodash');
 const mongoose = require('mongoose');
 
-const subComment = mongoose.model('subComment');
+const subComments = mongoose.model('subComment');
+const comments = mongoose.model('comment');
+const posts = mongoose.model('post');
 
 const { ObjectId } = mongoose.Types;
 
 class subCommentService {
   static getDetail() {
-    return subComment.find().exec();
+    return subComments.find().exec();
+  }
+  static createSubComment({ post, comment, user, content }){
+
+    return posts.findOneAndUpdate({_id: ObjectId(post)}, { $inc: { numOfComment:1 } , recentComment: content}, {new: true }).exec()
+      .then(() => subComments.create({ post: post, comment: comment, user: user, content: content })
+        .then((_subComment)=> comments.findOneAndUpdate({_id: comment}, { "$push": { "subComment": _subComment } }))
+        .then(
+          //console.log("im here");
+        ));
   }
 }
 module.exports = subCommentService;
