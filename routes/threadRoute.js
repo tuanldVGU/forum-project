121b52c5dab0094f89854b643179a76c946fa8a9
ducnhuan <ducnhuan246@gmail.com>
2018-10-19 22:42:08 +0700

    block access from visitor to forum or user to admin page

--- routes/threadRoute.js
+++ routes/threadRoute.js
@@ -1,28 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var loggedin = function (req, res, next) {
+    if (req.isAuthenticated()) {
+      next()
+    } else {
+      res.redirect('/signin')
+    }
+  }
 
-router.get('/', function(req,res,next){
+router.get('/',loggedin, function(req,res,next){
     res.render('./thread/forum',{title:"Welcome to Motor forum"});
 });
 
-router.get('/newforum', function(req,res,next){
+router.get('/newforum', loggedin,function(req,res,next){
     res.render('./thread/newForum',{title:"Motor || Create forum"});
 });
 
-router.get('/post', function(req,res,next){
+router.get('/post',loggedin, function(req,res,next){
     res.render('./thread/newPost',{title:"Motor || Post new thread"});
 });
 
-router.get('/thread', function(req,res,next){
+router.get('/thread',loggedin, function(req,res,next){
     console.log(req.params);
     res.render('./thread/thread',{title:"Motor || "});
 });
 
-router.get('/article', function(req,res,next){
+router.get('/article',loggedin, function(req,res,next){
     res.render('./thread/article',{title:"Motor || "});
 });
 
-router.get('/modifyPost', function(req,res,next){
+router.get('/modifyPost',loggedin, function(req,res,next){
     console.log(req.param);
     res.render('./thread/modifyPost',{title:"Motor || "});
 });
